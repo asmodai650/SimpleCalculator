710f307d476e7d9d0f7784c663797767ee4386e1
asmodai650 <asmodai650@gmail.com>
2021-03-13 18:40:19 -0600

    conditional statements

--- simpleCalculator/Program.cs
+++ simpleCalculator/Program.cs
@@ -10,37 +10,35 @@ namespace simpleCalculator
     {
         static void Main(string[] args)
         {
-            string someText = "Here is some text";
-            string someOtherText = "Here is some other text";
-
-            bool isEqual = someOtherText.Equals(someText, StringComparison.OrdinalIgnoreCase);
-
-            string addedText = someText + " " + someOtherText + " here is some more text.";
-            string formattedText = string.Format("{0} {1} here is some more text.", someText, someOtherText);
-
-            string anotherText = string.Empty; //use instead of string anotherText = "";
-
-            string replacedText = someText.Replace("text", "string");
-
-            Console.WriteLine(addedText);
-            Console.WriteLine(formattedText);
-
-            Console.WriteLine(someText);
-            Console.WriteLine(someText.Length);
-            Console.WriteLine(someText[8]);
-
-            Console.WriteLine(someText.Substring(8, 4));
-
-            Console.WriteLine(someText.ToLower());
-            Console.WriteLine(someText.ToUpper());
-            Console.WriteLine(someText.Substring(8, 4).ToUpper());
-
-            
+            string userName = Console.ReadLine().ToLower();
+            string password = Console.ReadLine().ToLower();
 
 
+            if (userName.Equals("danny") && password.Equals("741852"))
+            {
+                Console.WriteLine("Hello, Danny");
+            }
 
+            else if (userName.Equals("danny") && !password.Equals("741852"))
+            {
+                Console.WriteLine("Wrong password, idiot!");
+            }
 
+            else
+            {
+                Console.WriteLine("STRANGER DANGER!!!!");
+            }
 
+            //switch (userName)
+            //{
+            //    case "danny":
+            //        Console.WriteLine("Hello, Danny");
+            //        break;
+            //
+            //    default:
+            //        Console.WriteLine("Hello, stranger!");
+            //        break;
+            //}
 
             Console.ReadLine();
 
