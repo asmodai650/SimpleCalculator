a0b8acec6bb59fec9adbc23779263303ad2561a8
danny.curtis <danny.curtis@optum.com>
2021-03-25 19:51:40 -0500

    added inputconv test

--- SimpleCalculator/SimpleCalculator.Test.Unit/InputConverterTest.cs
+++ SimpleCalculator/SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -7,9 +7,15 @@ namespace SimpleCalculator.Test.Unit
     [TestClass]
     public class UnitTest1
     {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
         [TestMethod]
-        public void TestMethod1()
+        public void ConvertsValidStringInputToDouble()
         {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+
+            Assert.AreEqual(5, convertedNumber);
         }
     }
 }
